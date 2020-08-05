f22793f8f93fe64fc66a00e644619cfd8d7f2826
oksder <oksana.dergacova@gmail.com>
2020-08-05 12:36:55 +0100

    Changes logo's from png to svg

---
+++ dist/js/map-pin.js
@@ -0,0 +1,6 @@
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: iconBase + 'img/map-pin.png'
+  });
